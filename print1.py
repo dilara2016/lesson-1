a94eb6b620262f273073b8d945fa672b7d5e1f8f
dilara2016 <m.123.hunain@gmail.com>
2024-09-28 11:35:49 +0200

    print1

---
+++ print1.py
@@ -0,0 +1,8 @@
+# printing a greeting message
+print("hello,world")
+# printing my name
+print("my name is dilara")
+# printing a fun fact about my self
+print("i like playing soccer")
+#printing and preforming a calculacion
+print("the result of 3+5 is ", 3+5)
