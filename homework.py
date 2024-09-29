efb33795b074046ac489ce7d2019ca8bce90d89c
dilara2016 <m.123.hunain@gmail.com>
2024-09-29 10:51:15 +0200

    homework

---
+++ homework.py
@@ -0,0 +1,11 @@
+#printing a greting message
+print("welcome to my homework")
+
+#printing my name
+print("my name is dilara")
+
+#printing a fun fact 
+print("i like playing games")
+
+#printing and preformingn a calculacion
+print("the results of 9+6",9+6)
